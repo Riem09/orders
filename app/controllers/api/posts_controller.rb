f5ba4a3af29667d9362f346643a7c7892bbf36b4
Matthew Riemer <mattriemer36@gmail.com>
2015-06-14 13:23:16 +0200

    added the rest of the api

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,5 +4,54 @@ def index
 render json: Post.all
 end
 
+def show
+post = Post.find(params[:id])
+render json: post
+end
 
+def create
+post = Post.new(post_params)
+if post.save
+render status: 200, json: {
+  message: "Succesfully created post",
+  post: post,
+}.to_json
+else
+  render status: 422, json: {
+   errors: post.errors
+  }.to_json
+end
+end
+
+def update
+post = Post.find(params[:id])
+if post.update(post_params)
+  render json: {
+   status: 200,
+   message: "Succesfully updated",
+   post: post
+  }.to_json
+else
+ render json: {
+   status: 500,
+   message: "Update erorr",
+   post: post
+ }
+end
+end
+
+def destroy
+post = Post.find(params[:id])
+post.destroy
+render json: {
+status: 200,
+message: "Successfully deleted post",
+}.to_json
+end
+
+
+private
+def post_params
+  params.require(:todo_list).permit(:title, :description)
+end
 end
