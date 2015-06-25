0cbfa72c7d9b53dff116bb70680813de9f5af2c4
Matthew Riemer <mattriemer36@gmail.com>
2015-06-25 22:34:06 +0200

    added permissions for new cost

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :full_order, :extra, :users_id, :price)
+      params.require(:post).permit(:name, :full_order, :extra, :users_id, :total_cost)
     end
 end
